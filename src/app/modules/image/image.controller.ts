b5a096c4f62a968835697dedd04fd8165d972a95
safin33221 <safin33221@gmail.com>
2025-09-30 23:08:11 +0600

    Completed image controllers

--- src/app/modules/image/image.controller.ts
+++ src/app/modules/image/image.controller.ts
@@ -11,7 +11,54 @@ const uploadImage = async (req: Request, res: Response, next: NextFunction) => {
         return res.status(500).json(error)
     }
 }
+const GetAllImage = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const result = await imageServices.GetAllImage()
+        return res.status(201).json(result)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error)
+    }
+}
+const GetImageById = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id
+    try {
+
+        const result = await imageServices.GetImageById(id)
+        return res.status(201).json(result)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error)
+    }
+}
+const DeleteImage = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id
+    try {
+
+        const result = await imageServices.DeleteImage(id)
+        return res.status(201).json(result)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error)
+    }
+}
+const UpdateImage = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id
+    try {
+
+        const result = await imageServices.UpdateImage(id, req.body)
+        return res.status(201).json(result)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error)
+    }
+}
 
 export const imageController = {
-    uploadImage
+    uploadImage,
+    GetAllImage,
+    DeleteImage,
+    UpdateImage,
+    GetImageById
 }
